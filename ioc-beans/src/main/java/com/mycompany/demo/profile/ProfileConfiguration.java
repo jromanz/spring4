9318b8f5b0836a58c515f5e92106b5d8c7ca125f
Julio Roman <kalerguiz@gmail.com>
2015-02-27 22:51:38 -0500

    Configuración Profile y testing service 2

--- ioc-beans/src/main/java/com/mycompany/demo/profile/ProfileConfiguration.java
+++ ioc-beans/src/main/java/com/mycompany/demo/profile/ProfileConfiguration.java
@@ -8,57 +8,54 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Configuration	
-@Import({ DevelopmentDataSource.class })
+@Configuration
+@Import({DevelopmentDataSource.class})
 @EnableTransactionManagement
+@PropertySource("classpath:/config.properties")
+@ComponentScan({ "com.mycompany.demo.profile" })
 public class ProfileConfiguration {
 
 	private String entityPackage = "com.mycompany.demo.entity";
-
+	
 	@Autowired
 	private Environment environment;
 	@Autowired
 	private DataSource dataSource;
-
+	
 	@Bean
-	public LocalSessionFactoryBean sessionFactory(DataSource dataSource)
-			throws SQLException {
+	public LocalSessionFactoryBean sessionFactory(DataSource dataSource) throws SQLException{
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource);
-		sessionFactory.setPackagesToScan(new String[] { entityPackage });
+		sessionFactory.setPackagesToScan(new String[]{entityPackage});
 		sessionFactory.setHibernateProperties(hibernateProperties(environment));
 		return sessionFactory;
 	}
-
+	
 	private Properties hibernateProperties(Environment environment) {
 		Properties properties = new Properties();
-		properties.put("hibernate.hbm2ddl.auto",
-				environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
-		properties.put("hibernate.show_sql",
-				environment.getRequiredProperty("hibernate.show_sql"));
-		properties.put("hibernate.format_sql",
-				environment.getRequiredProperty("hibernate.format_sql"));
-		properties.put("hibernate.dialect",
-				environment.getRequiredProperty("hibernate.dialect"));
-		properties.put("hibernate.cache.user_query_cache", environment
-				.getRequiredProperty("hibernate.cache.user_query_cache"));
-		properties.put("javax.persistence.validation.mode", environment
-				.getRequiredProperty("javax.persistence.validation.mode"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql",environment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.dialect",environment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.cache.user_query_cache",environment.getRequiredProperty("hibernate.cache.user_query_cache"));
+		properties.put("javax.persistence.validation.mode", environment.getRequiredProperty("javax.persistence.validation.mode"));
 		return properties;
 	}
-
+	
 	@Bean
 	@Autowired
 	public HibernateTransactionManager transactionManager(SessionFactory s) {
-		HibernateTransactionManager txManager = new HibernateTransactionManager();
-		txManager.setSessionFactory(s);
-		return txManager;
+	       HibernateTransactionManager txManager = new HibernateTransactionManager();
+	       txManager.setSessionFactory(s);
+	       return txManager;
 	}
 }
